ab365383fb28f2273ba889c78ce3379ac06d08aa
Prashanth Thipparthi <34084731+createwithprashanth@users.noreply.github.com>
2025-09-20 09:02:29 +0400

    Fix VantaNet background and center alignment

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,26 @@ import VantaNet from './components/VantaNet';
 
 function App() {
   return (
-    <div className="relative min-h-screen w-full">
-      {/* Vanta Net Background */}
-      <VantaNet />
-      <div className="relative z-10 w-full max-w-4xl mx-auto px-4 text-center flex flex-col items-center justify-center min-h-screen">
-        <Header />
-        padding: '0 1rem',
-        background: 'transparent'
-        <Hero />
-        <About />
-        <Skills />
-        <Experience />
-        <Projects />
-        <Contact />
-        <Footer />
+    <div className="relative min-h-screen w-full overflow-hidden">
+      {/* Vanta Net Background - Fixed positioning to cover entire viewport */}
+      <div className="fixed inset-0 w-full h-full z-0">
+        <VantaNet />
+      </div>
+      
+      {/* Main Content - Centered and above background */}
+      <div className="relative z-10 min-h-screen w-full flex flex-col items-center justify-start">
+        <div className="w-full max-w-4xl mx-auto px-4 text-center">
+          <Header />
+          <main className="flex flex-col items-center justify-center text-center w-full">
+            <Hero />
+            <About />
+            <Skills />
+            <Experience />
+            <Projects />
+            <Contact />
+            <Footer />
+          </main>
+        </div>
       </div>
     </div>
   );
